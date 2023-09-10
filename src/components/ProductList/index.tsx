eccca3aaab634a3af7d2e0677abed1b5f8f75771
Adam Zajler <adamzajler01@gmail.com>
2023-09-10 21:28:33 +0200

    Added data-testid

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,12 +1,12 @@
-import { ProductItem } from "@components/ProductItem";
-import { type IProductListItem } from "@/types/IProductListItem";
+import {ProductItem} from "@components/ProductItem";
+import {type IProductListItem} from "@/types/IProductListItem";
 
-export const ProductList = ({ products }: { products: IProductListItem[] }) => {
-	return (
-		<div className="grid grid-cols-4 gap-4">
-			{products.map((product, index) => (
-				<ProductItem key={index} {...product} />
-			))}
-		</div>
-	);
+export const ProductList = ({products}: { products: IProductListItem[] }) => {
+    return (
+        <div className="grid grid-cols-4 gap-4" data-testid="products-list">
+            {products.map((product, index) => (
+                <ProductItem key={index} {...product} />
+            ))}
+        </div>
+    );
 };
